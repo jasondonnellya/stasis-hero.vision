7bf8da1a28b84f82f0a3c8e193266f1806af66da
Jason Donnelly <Jason.jimmy4@gmail.com>
2022-05-04 20:29:27 +0100

    Added comments to tailwindCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
+/**
+ *  require('tailwindcss')('tailwind.config.js'), // name your custom tailwind
+ *  require('autoprefixer')(),
+ */
+
 module.exports = {
     plugins: {
       tailwindcss: {},
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'media',
   theme: {
-    colors: {
-      primary: '#54ffc2',
-      secondary: '#fd56d1',
-      tertiary: '#181e43',
-      link: '#80f0ff',
-    },
     extend: {
       fontFamily: {
         'sans': ['eight-bit-dragon-font', ...defaultTheme.fontFamily.sans],
         'ancient' : ['ancient-modern-tales-font'],
         'dragon' : ['eight-bit-dragon-font']
+      },
+      colors: {
+        primary: '#54ffc2',
+        secondary: '#fd56d1',
+        tertiary: '#181e43',
+        link: '#80f0ff',
       },
     },
   },
